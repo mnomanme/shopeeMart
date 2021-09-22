09c6d7a19a799cb08d5cdd78353c0a41be5f9810
nomanme <abdullah.noman083@gmail.com>
2021-09-22 23:58:35 +0600

    component pass & logo add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header/Header';
 
 const App = () => {
 	return (
 		<div>
-			<h2>Start Shopee Mart</h2>
+			<Container>
+				<Header />
+			</Container>
 		</div>
 	);
 };
---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from '../../resources/images/logo.png';
+
+const Header = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-info rounded">
+			<div className="container-fluid">
+				<a className="navbar-brand" href="/">
+					<img className="w-25" src={logo} alt="logo" />
+				</a>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav">
+						<a className="nav-link active" href="/">
+							Home
+						</a>
+						<a className="nav-link active" href="/">
+							Features
+						</a>
+						<a className="nav-link active" href="/">
+							Pricing
+						</a>
+						<a className="nav-link active" href="/">
+							Disabled
+						</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Header;
