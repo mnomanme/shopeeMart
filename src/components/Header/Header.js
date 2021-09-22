23a1b747c66c0caa9a0efbf2b2d0f5505cb83b29
nomanme <abdullah.noman083@gmail.com>
2021-09-23 00:27:02 +0600

    navbar add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,34 +1,18 @@
 import React from 'react';
+import './Header.css';
 import logo from '../../resources/images/logo.png';
+import { Container } from 'react-bootstrap';
 
 const Header = () => {
 	return (
-		<nav className="navbar navbar-expand-lg navbar-light bg-info rounded">
-			<div className="container-fluid">
-				<a className="navbar-brand" href="/">
-					<img className="w-25" src={logo} alt="logo" />
-				</a>
-				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-					<span className="navbar-toggler-icon"></span>
-				</button>
-				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-					<div className="navbar-nav">
-						<a className="nav-link active" href="/">
-							Home
-						</a>
-						<a className="nav-link active" href="/">
-							Features
-						</a>
-						<a className="nav-link active" href="/">
-							Pricing
-						</a>
-						<a className="nav-link active" href="/">
-							Disabled
-						</a>
-					</div>
-				</div>
-			</div>
-		</nav>
+		<Container className="header">
+			<img src={logo} alt="" />
+			<nav>
+				<a href="/shop">Shop</a>
+				<a href="/order">Order</a>
+				<a href="/manage">Manage Inventory</a>
+			</nav>
+		</Container>
 	);
 };
 
