9770e60fc7662a96f60d0b892db77f3ffe0b0cf0
nomanme <abdullah.noman083@gmail.com>
2021-09-23 00:27:30 +0600

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import Header from './components/Header/Header';
 
 const App = () => {
 	return (
 		<div>
-			<Container>
-				<Header />
-			</Container>
+			<Header />
 		</div>
 	);
 };
