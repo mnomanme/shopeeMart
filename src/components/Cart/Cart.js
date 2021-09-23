2a2fadb9b1ac9015985de9fd2dea7ed850c76678
nomanme <abdullah.noman083@gmail.com>
2021-09-24 01:32:27 +0600

    total calculate

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = (props) => {
+	// console.log(props);
+	const { cart } = props;
+
+	let total = 0;
+	for (const product of cart) {
+		// console.log(product);
+		const { price } = product;
+		total += price;
+	}
 	return (
-		<div>
-			<h2>This is Cart</h2>
-		</div>
+		<section>
+			<h3>Order Summary</h3>
+			<h5>Items Ordered: {cart.length}</h5>
+			<p>Total: {total}</p>
+		</section>
 	);
 };
 
