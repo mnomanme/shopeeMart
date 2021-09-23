ec45ce30ec4ade8a939c313d43b0bad65f7ae225
nomanme <abdullah.noman083@gmail.com>
2021-09-23 22:31:05 +0600

    update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../resources/images/logo.png';
-import { Container } from 'react-bootstrap';
 
 const Header = () => {
 	return (
-		<Container className="header">
+		<section className="header">
 			<img src={logo} alt="" />
 			<nav>
 				<a href="/shop">Shop</a>
 				<a href="/order">Order</a>
 				<a href="/manage">Manage Inventory</a>
 			</nav>
-		</Container>
+		</section>
 	);
 };
 
