d1879bff6690e90f0eb311cd73828c118e942066
nomanme <abdullah.noman083@gmail.com>
2021-09-24 01:03:52 +0600

    cart component pass and button pass

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
 	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState(0);
 
 	useEffect(() => {
 		const url = `./products.json`;
@@ -20,18 +22,24 @@ const Shop = () => {
 		}
 	}, []);
 
+	// handleAddToCart
+
+	const handleAddToCart = (product) => {
+		console.log('add', product);
+	};
+
 	return (
 		<section className="shop-container">
 			<div className="product-container">
 				<h3>Products: {products.length} </h3>
 				{products.map((product) => {
 					// console.log(product);
-					return <Product product={product} />;
+					const { key } = product;
+					return <Product key={key} product={product} handleAddToCart={handleAddToCart} />;
 				})}
 			</div>
 			<div className="cart-container">
-				<h3>Order Summary: </h3>
-				<h5>Items Ordered: </h5>
+				<Cart />
 			</div>
 		</section>
 	);
