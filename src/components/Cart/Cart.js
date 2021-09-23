2f1852c28b6c2f60f9db59acbd78e2a72fd30b0f
nomanme <abdullah.noman083@gmail.com>
2021-09-24 01:52:01 +0600

    product calculation

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,17 +4,29 @@ const Cart = (props) => {
 	// console.log(props);
 	const { cart } = props;
 
-	let total = 0;
-	for (const product of cart) {
-		// console.log(product);
+	const total = cart.reduce((previous, product) => {
 		const { price } = product;
-		total += price;
-	}
+		return previous + price;
+	}, 0);
+
+	const shipping = 15;
+	const tax = (total + shipping) / 10;
+	const grandTotal = shipping + tax + total;
+
+	// let total = 0;
+	// for (const product of cart) {
+	// 	// console.log(product);
+	// 	const { price } = product;
+	// 	total += price;
+	// }
 	return (
 		<section>
 			<h3>Order Summary</h3>
 			<h5>Items Ordered: {cart.length}</h5>
-			<p>Total: {total}</p>
+			<p>Price: {total.toFixed(2)}</p>
+			<p>Shipping: {shipping.toFixed(2)}</p>
+			<p>Tax: {tax.toFixed(2)}</p>
+			<p>Total: {grandTotal.toFixed(2)}</p>
 		</section>
 	);
 };
