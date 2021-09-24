06fbc1057ec8f0d7f04efb9404e34a68db797add
nomanme <abdullah.noman083@gmail.com>
2021-09-24 23:33:50 +0600

    Nan resolved

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,27 +4,35 @@ import { Container } from 'react-bootstrap';
 const Cart = (props) => {
 	// console.log(props);
 	const { cart } = props;
+	console.log(cart);
 
-	const total = cart.reduce((previous, product) => {
-		const { price } = product;
-		return previous + price;
-	}, 0);
+	// reduce to calculate total
+	// const total = cart.reduce((previous, product) => {
+	// 	const { price } = product;
+	// 	return previous + price;
+	// }, 0);
+
+	let total = 0;
+	let totalItem = 0;
+	for (const product of cart) {
+		// console.log(product);
+		product.quantity = !product.quantity ? 1 : product.quantity;
+		// if (!product.quantity) {
+		// 	product.quantity = 1;
+		// }
+		const { price, quantity } = product;
+		total += price * +quantity;
+		totalItem += quantity;
+	}
 
 	const shipping = total > 0 ? 15 : 0;
 	const tax = (total + shipping) / 10;
 	const grandTotal = shipping + tax + total;
 
-	// let total = 0;
-	// for (const product of cart) {
-	// 	// console.log(product);
-	// 	const { price } = product;
-	// 	total += price;
-	// }
-
 	return (
 		<Container>
 			<h3>Order Summary</h3>
-			<h5>Items Ordered: {cart.length}</h5>
+			<h5>Items Ordered: {totalItem}</h5>
 			<p>Price: {total.toFixed(2)}</p>
 			<p>Shipping: {shipping.toFixed(2)}</p>
 			<p>Tax: {tax.toFixed(2)}</p>
