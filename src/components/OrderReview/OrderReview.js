9278deb80bb3be4ebc9442da85986aeafbc7e199
nomanme <abdullah.noman083@gmail.com>
2021-10-08 19:51:23 +0600

    review length check

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 
 const OrderReview = () => {
+	const [products] = useProducts();
+
 	return (
 		<div>
-			<h2>Order Review</h2>
+			<h2>Order Review {products.length}</h2>
 		</div>
 	);
 };
