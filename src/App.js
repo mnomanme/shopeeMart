e32bb4fd5ca342bc1bfe6fca6e19546df6a7fbe9
nomanme <abdullah.noman083@gmail.com>
2021-10-08 15:53:07 +0600

    router add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound/NotFound';
 
 const App = () => {
 	return (
 		<Container>
-			<Header />
-			<Shop />
+			<Router>
+				<Header />
+				<Switch>
+					<Route path="/shop">
+						<Shop />
+					</Route>
+					<Route path="/review">
+						<OrderReview />
+					</Route>
+					<Route path="/inventory">
+						<Inventory />
+					</Route>
+					<Route exact path="/">
+						<Shop />
+					</Route>
+					<Route path="*">
+						<NotFound />
+					</Route>
+				</Switch>
+			</Router>
 		</Container>
 	);
 };
