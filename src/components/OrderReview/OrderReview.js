f1bccf6025355ced83aaba9bbc5c4f8bde7ce6e1
nomanme <abdullah.noman083@gmail.com>
2021-10-08 20:11:19 +0600

    cart item show

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
+import useCart from '../../hooks/useCart';
 
 const OrderReview = () => {
 	const [products] = useProducts();
+	const [cart] = useCart(products);
 
 	return (
 		<div>
 			<h2>Order Review {products.length}</h2>
+			<h5>Cart {cart.length}</h5>
 		</div>
 	);
 };
