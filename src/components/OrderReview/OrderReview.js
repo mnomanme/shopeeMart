dc42b9fca53117ca698dfe2a407f0a9baa9240e2
nomanme <abdullah.noman083@gmail.com>
2021-10-08 20:26:55 +0600

    cart review summary

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import useProducts from '../../hooks/useProducts';
 import useCart from '../../hooks/useCart';
+import Cart from '../Cart/Cart';
 
 const OrderReview = () => {
 	const [products] = useProducts();
@@ -8,8 +9,14 @@ const OrderReview = () => {
 
 	return (
 		<div>
-			<h2>Order Review {products.length}</h2>
-			<h5>Cart {cart.length}</h5>
+			<section className="shopContainer">
+				<div className="productContainer">
+					<h3>Cart: {cart.length} </h3>
+				</div>
+				<div className="cart-container">
+					<Cart cart={cart} />
+				</div>
+			</section>
 		</div>
 	);
 };
