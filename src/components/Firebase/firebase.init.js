e4b1b3cb29ab01a6230e781d2ed1ce33ce17285e
nomanme <abdullah.noman083@gmail.com>
2021-10-14 00:42:25 +0600

    initialize firebase

--- src/components/Firebase/firebase.init.js
+++ src/components/Firebase/firebase.init.js
@@ -9,11 +9,15 @@ export default initializeAuthentication;
 
 /* steps for authentication
 
-initial setup 
+step-1: initial setup 
 1. firebase: create project 
 2. create web app 
 3. get configuration
 4. initialize firebase
 5. enable auth methdod  
 
+step-2:
+1. create login component 
+2. create register component 
+3. create route for login and registration
 */
