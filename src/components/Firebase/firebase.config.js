7d017a8af5d5f9f22a0e21badd3bc18eece0d955
nomanme <abdullah.noman083@gmail.com>
2021-10-13 22:22:08 +0600

    firebase initialize

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyC0blrR206RVVjyPHRTHZea_EzIOB57eUc',
+	authDomain: 'shopee-mart.firebaseapp.com',
+	projectId: 'shopee-mart',
+	storageBucket: 'shopee-mart.appspot.com',
+	messagingSenderId: '520271220604',
+	appId: '1:520271220604:web:8f8b7f6ea5293ba7bf1ef9',
+};
+export default firebaseConfig;
