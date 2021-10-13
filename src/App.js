7b22ba68ef7daf84c167d95c7fb16350f5bacf40
nomanme <abdullah.noman083@gmail.com>
2021-10-13 22:21:45 +0600

    path check

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
 					<Route path="/inventory">
 						<Inventory />
 					</Route>
-					<Route to="/placeorder">
+					<Route path="/placeorder">
 						<PlaceOrder />
 					</Route>
 					<Route exact path="/">
