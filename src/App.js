3bdfb6ba30e46f6ed888aa1130f7a839872e09e3
nomanme <abdullah.noman083@gmail.com>
2021-10-15 00:01:25 +0600

    set auth provider

--- src/App.js
+++ src/App.js
@@ -9,39 +9,42 @@ import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 import NotFound from './components/NotFound/NotFound';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import AuthProvider from './context/AuthProvider';
 
 const App = () => {
 	return (
 		<Container>
-			<Router>
-				<Header />
-				<Switch>
-					<Route path="/shop">
-						<Shop />
-					</Route>
-					<Route path="/review">
-						<OrderReview />
-					</Route>
-					<Route path="/inventory">
-						<Inventory />
-					</Route>
-					<Route path="/placeorder">
-						<PlaceOrder />
-					</Route>
-					<Route exact path="/">
-						<Shop />
-					</Route>
-					<Route path="/login">
-						<Login />
-					</Route>
-					<Route path="/register">
-						<Register />
-					</Route>
-					<Route path="*">
-						<NotFound />
-					</Route>
-				</Switch>
-			</Router>
+			<AuthProvider>
+				<Router>
+					<Header />
+					<Switch>
+						<Route path="/shop">
+							<Shop />
+						</Route>
+						<Route path="/review">
+							<OrderReview />
+						</Route>
+						<Route path="/inventory">
+							<Inventory />
+						</Route>
+						<Route path="/placeorder">
+							<PlaceOrder />
+						</Route>
+						<Route exact path="/">
+							<Shop />
+						</Route>
+						<Route path="/login">
+							<Login />
+						</Route>
+						<Route path="/register">
+							<Register />
+						</Route>
+						<Route path="*">
+							<NotFound />
+						</Route>
+					</Switch>
+				</Router>
+			</AuthProvider>
 		</Container>
 	);
 };
