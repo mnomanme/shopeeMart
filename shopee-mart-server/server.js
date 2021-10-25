d2f1f19582714689a41b41fc2da9c3897e86d3b8
nomanme <abdullah.noman083@gmail.com>
2021-10-25 19:07:32 +0600

    database connected successfully

--- shopee-mart-server/server.js
+++ shopee-mart-server/server.js
@@ -1,20 +1,42 @@
 const express = require('express');
+
+const cors = require('cors');
+
 const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@shopeemart.uz4v8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const app = express();
+async function run() {
+	try {
+		await client.connect();
+		console.log('Shopee Mart Database Connected Successfully!');
+		const database = client.db('db1shopee');
+		const haiku = database.collection('productsItem');
 
-const port = 4000;
+		// const result = await haiku.insertOne(doc);
+		// console.log(`A document was inserted with the _id: ${result.insertedId}`);
+	} finally {
+		// await client.close();
+	}
+}
+run().catch(console.dir);
 
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
 	res.send('Shoppe Mart Is Here!');
 });
 
-app.listen(port, () => {
+app.listen(4000, () => {
 	console.log(`Listening The PORT ${port}`);
 });
